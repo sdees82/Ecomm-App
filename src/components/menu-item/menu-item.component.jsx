c20a1bfb7fe9cca739b8cef276308e25e6bf4bb7
sdees82 <seantedees1982@gmail.com>
2019-07-02 09:24:14 -0400

    added more styling to home page

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -2,7 +2,8 @@ import React from 'react'
 import './menu-item.styles.scss';
 
 const  MenuItem = ({title, imageUrl, size}) => (
-       <div style={{backgroundImage: `url(${imageUrl})`}} className={`${size} menu-item`}>
+       <div className={`${size} menu-item`}>
+           <div style={{backgroundImage: `url(${imageUrl})`}} className="background-image"/>
             <div className="content">
                 <h1 className="title">{title.toUpperCase()}</h1>
                 <span className="subtitle">Shop Now</span>
