176bd61667b58d1d780e3794cfc27b661870fd7c
sdees82 <seantedees1982@gmail.com>
2019-07-02 11:26:37 -0400

    updated menu items to nagivgate with withRouter HOC

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -11,37 +11,42 @@ class Directory extends Component {
             sections: [{
                 title: 'hats',
                 imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
-                id: 1
+                id: 1,
+                linkUrl: 'hats'
             },
             {
                 title: 'Jackets',
                 imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
-                id: 2
+                id: 2,
+                linkUrl: ''
             },
             {
                 title: 'Sneakers',
                 imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
-                id: 3
+                id: 3,
+                linkUrl: ''
             },
             {   
                 title: 'Womens',
                 imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
                 size: 'large',
-                id: 4
+                id: 4,
+                linkUrl: ''
             },
             {
                 title: 'Mens',
                 imageUrl: 'https://i.ibb.co/R70vBrQ/mens.png',
                 size: 'large',
-                id: 5
+                id: 5,
+                linkUrl: ''
             }]
         }
     }
     render() {
         return (
             <div className="directory-menu">
-                {this.state.sections.map(({title, imageUrl, id, size}) => (
-                    <MenuItem key={id} title={title} imageUrl={imageUrl} size={size}/>
+                {this.state.sections.map(({id, ...otherSectionProps}) => (
+                    <MenuItem key={id} {...otherSectionProps}/>
                 ))}
                 </div>
         );
